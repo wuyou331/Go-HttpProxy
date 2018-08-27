6983e4b88e22e1c018947cc8cd04b6ea933251db
wuyou <wuyou331@gmail.com>
2018-08-27 20:15:45 +0800

    完成http代理协议转换，但client会自动302跳转，需改为socket直连方式。

--- main.go
+++ main.go
@@ -1,89 +1,48 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
+	"bufio"
 	"log"
 	"net"
 	"net/http"
-	"net/url"
-	"strings"
+	"strconv"
 )
 
-func proxy(client net.Conn) {
-	if client == nil {
+func proxy(conn net.Conn) {
+	if conn == nil {
 		return
 	}
-	defer client.Close()
-
-	body, err := ioutil.ReadAll(client)
+	defer conn.Close()
+	req, err := http.ReadRequest(bufio.NewReader(conn))
 	if err != nil {
-		log.Println(err)
-	}
-
-	var method, host string
-	var header = getHeader(body)
-
-	fmt.Sscanf(header[0][1], "%s%s", &method, &host)
-	hostPortURL, err := url.Parse(host)
-	if err != nil {
-		log.Println(err)
 		return
 	}
-	if hostPortURL.Opaque == "443" {
 
+	if req.Method == "CONNECT" {
+
+	} else {
+		if req.Method == "POST" {
+			cl := req.Header.Get("Content-Length")
+			req.Header.Add("Connection", "keep-alive")
+			length, _ := strconv.Atoi(cl)
+
+			if length == 0 {
+				log.Panic("内容长度不一致")
+			}
+		}
+		req.Header.Del("Proxy-Connection")
+		req.RequestURI = ""
+		client := &http.Client{}
+
+		resp, respErr := client.Do(req)
+		if respErr != nil {
+			return
+		}
+		defer resp.Body.Close()
+
+		resp.Write(conn)
 	}
 
-	http.NewRequest(method, host, strings.NewReader(""))
-	//if err != nil {
-	//	// handle error
-	//}
-	//r.Header.Del("Proxy-Connection")
-	//req.Header=r.Header
-	//
-	//resp, err := client.Do(req)
-	//
-	//defer resp.Body.Close()
-	//rHead := w.Header()
-	//for h:= range resp.Header{
-	//
-	//	rHead.Add(h,resp.Header.Get(h))
-	//}
-	//
-	//body, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	// handle error
-	//}
-	//
-	//w.Write(body)
-}
-
-func getHeader(body []byte) map[string][]string {
-	var req *http.Request
-
-	var index = 0
-	var header = make(map[string][]string)
-	for {
-		end := bytes.IndexAny(body[index:], "\r\n")
-		if end == -1 {
-			break
-		}
-		txt := string(body[index : index+end])
-		if len(txt) == 0 {
-			break
-		}
-		var s []string
-		if len(header) == 0 {
-			s = strings.Split(txt, " ")
-		} else {
-			s = strings.Split(txt, ":")
-		}
-		header[s[0]] = s[1:]
-
-		index = index + end + 2
-	}
-	return header
 }
 
 func main() {
@@ -91,6 +50,7 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
 	for {
 		client, err := l.Accept()
 		if err != nil {
@@ -98,5 +58,4 @@ func main() {
 		}
 		proxy(client)
 	}
-
 }
