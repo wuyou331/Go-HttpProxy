41c23fedca2538b713e05ec27b9ddafc44b93c08
wuyou <wuyou331@gmail.com>
2018-08-28 12:25:30 +0800

    该进程自定义http解析

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 )
@@ -20,60 +20,117 @@ func proxy(conn net.Conn) {
 	}
 	defer conn.Close()
 
-	connBuf := bufio.NewReader(conn)
-	req, err := http.ReadRequest(connBuf)
-	if err != nil {
-		return
+	var header = make(map[string]string)
+	var method, proto string
+	var host *url.URL
+
+	var connReader = bufio.NewReader(conn)
+	//读取请求头第一行
+	if line, err := readLine(connReader); err != nil {
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+	} else {
+
+		args := strings.Split(line, " ")
+		if len(args) != 3 {
+			//非标准请求头
+			fmt.Printf("首行非标准请求头:%s", line)
+			return
+		}
+		method, proto = args[0], args[2]
+		host, err = url.Parse(args[1])
+		if err != nil {
+			fmt.Printf("Host非标准URL:%s", args[1])
+			return
+		}
+	}
+	//组装header
+	for {
+		line, err := readLine(connReader)
+		if err != nil {
+			fmt.Print(err)
+			return
+		} else if len(line) == 0 {
+			break
+		}
+		spitIndex := strings.IndexAny(line, ":")
+		if spitIndex == -1 {
+			//非标准请求头
+			fmt.Printf("非标准请求头:%s", line)
+			return
+		}
+		header[line[0:spitIndex]] = strings.Trim(line[spitIndex+1:], " ")
 	}
 
-	if req.Method == "CONNECT" {
+	//发起请求
+	if method == "CONNECT" {
 
 	} else {
-		req.Header.Del("Proxy-Connection")
-		req.Header.Add("Connection", "close")
+		//修改请求头，不保持tcp连接
+		delete(header, "Proxy-Connection")
+		delete(header, "Connection")
+		header["Connection"] = "close"
 
 		//发起http请求
-		client, _ := net.Dial("tcp", req.Host)
+		httpClient, err := net.Dial("tcp", host.Host)
+		if err != nil {
+			fmt.Printf("服务器无法连接:%s", host.Host)
+			return
+		}
 		defer func() {
-			if client != nil {
-				client.Close()
+			if err := recover(); err != nil {
+				fmt.Println(err)
+			}
+			if httpClient != nil {
+				httpClient.Close()
 			}
 		}()
 
-		reqWriter := bufio.NewWriter(client)
+		reqWriter := bufio.NewWriter(httpClient)
 		//写入头部
-		reqWriter.WriteString(fmt.Sprintf("%s %s %s%s", req.Method, req.URL.Path, req.Proto, NewLine))
-		reqWriter.WriteString(fmt.Sprintf("Host: %s%s", req.Host, NewLine))
-		for head := range req.Header {
-			reqWriter.WriteString(fmt.Sprintf("%s: %s%s", head, strings.Join(req.Header[head], ","), NewLine))
+		reqWriter.WriteString(fmt.Sprintf("%s %s %s%s", method, host.Path, proto, NewLine))
+		for head := range header {
+			reqWriter.WriteString(fmt.Sprintf("%s: %s%s", head, header[head], NewLine))
 		}
 		reqWriter.WriteString(NewLine)
-		cl := req.Header.Get("Content-Length")
-		length, _ := strconv.Atoi(cl)
-		if length > 0 {
-			//写入body
-			body, _ := connBuf.Peek(length)
-			reqWriter.Write(body)
+		if cl, ok := header["Content-Length"]; ok {
+			//获取body长度，写入	body
+			if len, _ := strconv.Atoi(cl); len > 0 {
+				buf, _ := connReader.Peek(len)
+
+				reqWriter.Write(buf[0:len])
+			}
 		}
 
-		if client != nil {
+		//有时候数据写完，socket即断开连接
+		if httpClient != nil {
 			reqWriter.Flush()
-			rspReader := bufio.NewReader(client)
-			var buf = make([]byte, 1024*10)
+			rspReader := bufio.NewReader(httpClient)
+			var buf = make([]byte, 1024*100)
 			for {
-				length, err = rspReader.Read(buf)
-				if length == 0 || err != nil {
+				len, err := rspReader.Read(buf)
+				if len == 0 || err != nil {
 					break
 				}
-				conn.Write(buf[0:length])
+				conn.Write(buf[0:len])
 			}
-
 		}
 
 	}
 
 }
 
+func readLine(r *bufio.Reader) (string, error) {
+	line, err := r.ReadString('\n')
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Print(err)
+		}
+	}()
+	return line[:len(line)-2], err
+}
 func main() {
 	l, err := net.Listen("tcp", ":1081")
 	if err != nil {
